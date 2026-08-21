194e490ec2f8c33e238bd633456a6c3f6c5ae04d
Lauratro <lauratronchin@hotmail.it>
2026-08-21 09:48:19 +0200

    Add prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+
+  prettier,
+];
