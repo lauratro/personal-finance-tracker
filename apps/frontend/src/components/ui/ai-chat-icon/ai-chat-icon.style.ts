3726e23f4900808752f3758b4997525b389cceac
Lauratro <lauratronchin@hotmail.it>
2026-09-04 11:58:42 +0200

    Add icon for chat in ai chat icon

---
+++ apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AiChatIconContainer = styled.div`
+  position: fixed;
+  z-index: 1000;
+  bottom: 20px;
+  right: 20px;
+`;
---
+++ apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.tsx
@@ -0,0 +1,10 @@
+import { AiChatIconContainer } from './ai-chat-icon.style';
+import { IconSubtitlesAi } from '@tabler/icons-react';
+
+export const AiChatIcon = () => {
+  return (
+    <AiChatIconContainer>
+      <IconSubtitlesAi stroke={2} />
+    </AiChatIconContainer>
+  );
+};
