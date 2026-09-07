9b9a7799e5fb9b49ef77a6de5a39e53889d35875
Lauratro <lauratronchin@hotmail.it>
2026-09-07 11:06:32 +0200

    Define ai icon container

--- apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.style.ts
+++ apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.style.ts
@@ -9,4 +9,13 @@ export const AiChatIconContainer = styled.div`
   background-color: var(--primary);
   padding: 10px;
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AiChatIconButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
 `;
--- apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.tsx
+++ apps/frontend/src/components/ui/ai-chat-icon/ai-chat-icon.tsx
@@ -1,10 +1,14 @@
-import { AiChatIconContainer } from './ai-chat-icon.style';
+import { AiChatIconButton, AiChatIconContainer } from './ai-chat-icon.style';
 import { IconSubtitlesAi } from '@tabler/icons-react';
+import React, { useState } from 'react';
 
 export const AiChatIcon = () => {
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <AiChatIconContainer>
-      <IconSubtitlesAi stroke={2} color="white" />
+      <AiChatIconButton onClick={() => setIsVisible(!isVisible)}>
+        <IconSubtitlesAi stroke={2} color="white" />
+      </AiChatIconButton>
     </AiChatIconContainer>
   );
 };
