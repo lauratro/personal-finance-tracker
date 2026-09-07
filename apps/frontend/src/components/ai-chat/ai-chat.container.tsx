bc50a0d7755cdb7f1d1a0a738ea2f99df2d9cc0d
Lauratro <lauratronchin@hotmail.it>
2026-09-07 13:41:44 +0200

    Create chatbox

--- apps/frontend/src/components/ai-chat/ai-chat.container.tsx
+++ apps/frontend/src/components/ai-chat/ai-chat.container.tsx
@@ -1,7 +1,15 @@
 import { AiChatIcon } from './parts/ai-chat-icon';
 import React, { useState } from 'react';
+import { AiChatBox } from './parts/ai-chat-box';
+import { AiChatContainerStyle } from './ai-chat-container.style';
 
 export const AiChatBoxContainer = () => {
   const [isVisible, setIsVisible] = useState(false);
-  return <AiChatIcon isVisible={isVisible} setIsVisible={setIsVisible} />;
+
+  return (
+    <AiChatContainerStyle>
+      <AiChatIcon isVisible={isVisible} setIsVisible={setIsVisible} />
+      {isVisible && <AiChatBox opened={isVisible} setOpened={setIsVisible} />}
+    </AiChatContainerStyle>
+  );
 };
